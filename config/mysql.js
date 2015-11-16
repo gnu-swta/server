86478fa252535b7a9a09b204713feffd61e1499e
JeonChangwoo <rainsy02@gmail.com>
2015-11-16 21:59:52 +0900

    mysql.js 수정

--- config/mysql.js
+++ config/mysql.js
@@ -1,26 +1,26 @@
 /**
  * Created by jeon on 15. 11. 8..
  */
-//module.exports =
-//{
-//    host    :'ja-cdbr-azure-west-a.cloudapp.net',
-//    port : 3306,
-//    user : 'bd84d84c29cda5',
-//    password : '63370122',
-//    database:'swta',
-//    connectionLimit:1,
-//    multipleStatements: true,
-//    waitForConnections:true
-//};
-
 module.exports =
 {
-    host    :'localhost',
+    host    :'ja-cdbr-azure-west-a.cloudapp.net',
     port : 3306,
-    user : 'jeon',
-    password : 'wpdjvks',
+    user : 'bd84d84c29cda5',
+    password : '63370122',
     database:'swta',
-    connectionLimit:20,
+    connectionLimit:1,
     multipleStatements: true,
     waitForConnections:true
 };
+
+//module.exports =
+//{
+//    host    :'localhost',
+//    port : 3306,
+//    user : 'jeon',
+//    password : 'wpdjvks',
+//    database:'swta',
+//    connectionLimit:20,
+//    multipleStatements: true,
+//    waitForConnections:true
+//};
